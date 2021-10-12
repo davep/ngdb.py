170b42cbf248e5324710c2773337a5a3382263b4
Dave Pearson <davep@davep.org>
2021-10-12 21:47:25 +0100

    :clipboard: Add a less-trivial guide for testing

--- tests/__init__.py
+++ tests/__init__.py
@@ -8,6 +8,7 @@ from pathlib import Path
 # Test database names.
 GUIDES_BASE   = Path( __name__ ).resolve() / "guides"
 GOOD_GUIDE    = GUIDES_BASE / "eg.ng"
+BIG_GUIDE     = GUIDES_BASE / "oslib.ng"
 MISSING_GUIDE = GUIDES_BASE / "does-not-exist.ng"
 
 ### __init__.py ends here
