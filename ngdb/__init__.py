714fe252209d1e6d48b509c7704af86aefe3f4b5
Dave Pearson <davep@davep.org>
2022-01-07 16:42:22 +0000

    :bookmark: v0.2.0

--- ngdb/__init__.py
+++ ngdb/__init__.py
@@ -3,11 +3,11 @@
 ######################################################################
 # Main library information.
 __author__     = "Dave Pearson"
-__copyright__  = "Copyright 2021, Dave Pearson"
+__copyright__  = "Copyright 2021-2022, Dave Pearson"
 __credits__    = [ "Dave Pearson" ]
 __maintainer__ = "Dave Pearson"
 __email__      = "davep@davep.org"
-__version__    = "0.1.1"
+__version__    = "0.2.0"
 __licence__    = "GPLv3+"
 
 ##############################################################################
