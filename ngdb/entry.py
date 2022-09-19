105c5db3ef3a4a1f774e1ecce69401fcfbbe2abf
Dave Pearson <davep@davep.org>
2022-09-19 20:19:44 +0100

    :art: Harmonise how I test for "there is no offset" (redux)
    I'd forgotten that I've run into instances where "there is no offset" in a
    guide can me 0 or -1. Eh, don't ask, DOS days were fun days okay? Anyway,
    testing that we're > 0 does the job so let's go with that.

--- ngdb/entry.py
+++ ngdb/entry.py
@@ -39,7 +39,7 @@ class EntryParent:
         :returns: ``True`` if there is, ``False` if not.
         :rtype: bool
         """
-        return self.offset != -1
+        return self.offset > 0
 
     @staticmethod
     def _non_test( value: int ) -> int:
@@ -237,7 +237,7 @@ class Entry:
 
         :type: bool
         """
-        return self.previous > -1
+        return self.previous > 0
 
     @property
     def next( self ) -> int:
@@ -253,7 +253,7 @@ class Entry:
 
         :type: bool
         """
-        return self.next > -1
+        return self.next > 0
 
     @property
     def lines( self ) -> Tuple[ str, ... ]:
@@ -335,7 +335,7 @@ class Short( Entry ):
 
             :type: bool
             """
-            return self.offset > -1
+            return self.offset > 0
 
     def __getitem__( self, line: int ) -> Line:
         """Get a line and its offset."""
