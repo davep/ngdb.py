2276777933cf46cd8c55254e1e95f4f01fdce028
Dave Pearson <davep@davep.org>
2022-10-21 07:05:53 +0100

    :books: Convert the link code to Google style docstring

--- ngdb/link.py
+++ ngdb/link.py
@@ -9,29 +9,39 @@ from typing import NamedTuple
 class Link( NamedTuple ):
     """A link within a Norton Guide, comprising of some text and an offset."""
 
-    #: The text of the link.
     text: str
-    #: The offset of the link.
+    """str: The text of the link."""
+
     offset: int
+    """int: The offset of the link."""
 
     def __str__( self ) -> str:
-        """The text of the link."""
+        """Returns the text of the link.
+
+        Returns:
+            str: The text for the link.
+        """
         return self.text
 
     def __int__( self ) -> int:
-        """The offset of the link."""
+        """Returns the offset of the link.
+
+        Returns:
+            int: The offset for the link.
+        """
         return self.offset
 
     @property
     def has_offset( self ) -> bool:
-        """Does this link have an associated offset into the guide?
-
-        :type: bool
-        """
+        """bool: Does this link have an associated offset into the guide?"""
         return self.offset > 0
 
     def __bool__( self ) -> bool:
-        """Does the link actually link anywhere?"""
+        """Does the link actually link anywhere?
+
+        Returns:
+            bool: ``True`` if the link links somewhere, ``False`` if not.
+        """
         return self.has_offset
 
 ### link.py ends here
