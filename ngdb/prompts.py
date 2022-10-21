7d7dbb31d1f408ecb6e09adebcbd504e77926c64
Dave Pearson <davep@davep.org>
2022-10-21 09:11:34 +0100

    :books: Convert the prompt collection code to Google style docstring

--- ngdb/prompts.py
+++ ngdb/prompts.py
@@ -13,8 +13,8 @@ from .link import Link
 class PromptCollection:
     """Base class for classes that contain prompt/offset collections."""
 
-    #: The maximum length of a prompt in a guide.
     MAX_PROMPT_LENGTH: Final = 128
+    """The maximum length of a prompt in a guide."""
 
     def __init__( self ) -> None:
         """Constructor."""
@@ -24,48 +24,43 @@ class PromptCollection:
 
     @property
     def prompts( self ) -> Tuple[ str, ... ]:
-        """The prompts in the collection.
-
-        :type: Tuple[str,...]
-        """
+        """Tuple[str,...]: The prompts in the collection."""
         return self._prompts
 
     @property
     def offsets( self ) -> Tuple[ int, ... ]:
-        """The offsets into the guide for each prompt.
-
-        :type: Tuple[int,...]
-        """
+        """Tuple[int,...]: The offsets into the guide for each prompt."""
         return self._offsets
 
     def __len__( self ) -> int:
         """Get the number of prompts in the collection.
 
-        :returns: The count of prompts.
-        :rtype: int
+        Returns:
+            int: The count of prompts.
         """
         return self._count
 
     def __getitem__( self, prompt: int ) -> Link:
         """Get a prompt/offset pair.
 
-        :returns: The requested prompt and its offset.
-        :rtype: Link
+        Returns:
+            Link: The requested prompt and its offset.
         """
         return Link( self.prompts[ prompt ], self.offsets[ prompt ] )
 
     def __iter__( self ) -> Iterator[ Link ]:
         """Get an iterator of prompt and offset pairs.
 
-        :yields: Link
+        Yields:
+            Link: A link containing the prompt/offset, from the collection.
         """
         return ( Link( *prompt ) for prompt in zip( self.prompts, self.offsets ) )
 
     def __bool__( self ) -> bool:
         """Test if there are any prompts in the collection.
 
-        :returns: ``True`` if there are prompts, ``False`` if not.
-        :rtype: bool
+        Returns:
+            bool: ``True`` if there are prompts, ``False`` if not.
         """
         return bool( len( self ) )
 
